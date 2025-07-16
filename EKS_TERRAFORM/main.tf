b146baa2fd88e4e079c241d4ff738b6aeab747b2
Naga Srinivasu Rowtula <68268949+kalyanvasu821@users.noreply.github.com>
2025-07-16 16:37:49 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,10 +1,10 @@
 resource "aws_eks_cluster" "my_cluster" {
   name     = "my-cluster"
-  role_arn = "arn:aws:iam::905418143553:role/ec2-0admin"  # Replace with your IAM role ARN
+  role_arn = "arn:aws:iam::881490093601:role/ec2-uber"  # Replace with your IAM role ARN
 
   vpc_config {
-    subnet_ids         = ["subnet-0a302525a470342cf", "subnet-09fe2ebc7d25c14dd"]  # Replace with your subnet IDs
-    security_group_ids = ["sg-0aff926e8c0eec7af"]                         # Replace with your security group IDs
+    subnet_ids         = ["subnet-08048b6393180a941", "subnet-064848ae0dfad2e4d"]  # Replace with your subnet IDs
+    security_group_ids = ["sg-05e385a6b044cbaf7"]                         # Replace with your security group IDs
   }
 
   tags = {
@@ -15,8 +15,8 @@ resource "aws_eks_cluster" "my_cluster" {
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
-  node_role_arn   = "arn:aws:iam::905418143553:role/ec2-0admin"  # Replace with your IAM role ARN
-  subnet_ids         = ["subnet-0a302525a470342cf", "subnet-09fe2ebc7d25c14dd"]      # Replace with your subnet IDs
+  node_role_arn   = "arn:aws:iam::881490093601:role/ec2-uber"  # Replace with your IAM role ARN
+  subnet_ids         = ["subnet-08048b6393180a941", "subnet-064848ae0dfad2e4d"]      # Replace with your subnet IDs
 
   scaling_config {
     desired_size = 1
